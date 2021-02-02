2b6452739e593fae40d75c04f14f9e42dea89a1b
seatop <seatop@126.com>
2021-02-02 23:18:05 +0800

    update

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ $(function(){
 		return Math.random()>0.5?-1:1;
 	}
 
-	numstr='空退.#_+/*$@0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ';	
+	numstr='.#_+/*$@0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ空退';	
 	nsarr=numstr.split("");
 	outss='<div id="s_all"><div id="s_fonts">';
 	for(i=0;i<nsarr.length;i++){
@@ -35,21 +35,21 @@ $(function(){
 	}		
 	outss+='</div></div>';
 
-	outinput='<div id="p_ss"><ul><li>输入密码关键词 <input type="password" id="ps1" /></li><li>重复密码关键词 <input type="password" id="ps2" /></li><li><h1 id="btps">确定</h1></li></ul></div>';
+	outinput='<div id="p_ss"><ul><li>输入密码关键词&nbsp;&nbsp;<input type="password" id="ps1" /></li><li>重复密码关键词&nbsp;&nbsp;<input type="password" id="ps2" /></li><li><h1 id="btps">确定</h1></li></ul></div>';
 
-	outinputk='<div id="p_ss"><ul><li>输入附加关键词 <input type="text" id="psk" /></li><li><h1 id="btpsk">确定</h1></li></ul></div>';
+	outinputk='<div id="p_ss"><ul><li>输入附加关键词&nbsp;&nbsp;<input type="text" id="psk" /></li><li><h1 id="btpsk">确定</h1></li></ul></div>';
 
 	outinputl='<div id="p_ss"><ul><li><h2 id="ps_l">当前密码长度为： '+pswdl+'</h2></li></ul></div>';
 
 	outn='<div id="s_all"><div id="s_fonts">';
-	for(i=4;i<31;i++){
+	for(i=4;i<40;i++){
 		outn+='<div class="n_font">'+i+'</div>';
 	}		
 	outn+='</div></div>';
 
-	$("#app-right").html(outinput+outss);
+	$("#app-content").html(outinput+outss);
 
-	$("#app-left li:eq(0)").css({"background":"#bcd","color":"#789"});
+	$("nav li:eq(0)").css({"background":"#dcd","color":"#456"});
     
 	$("#ps1").focus();
 
@@ -70,10 +70,10 @@ $(function(){
 		$("#btpsk").html("确定");
 	});
     
-    $("#about").click(function(){
-    	$("#app-left li").css({"background":"#789","color":"#bcd"});
-        outabout='<div id="p_ss"><ul><li id="aboutt">关于</li><li class="aboutl">用记忆中的关键词生成无意义密码。</li><li class="aboutl">纯JS程序，MD5多重加密，可存至本地运行。</li><li class="aboutl">密码关键词为空时，生成随机密码。</li><li class="aboutl">附加关键词可用于区分不同的用途。</li><li class="aboutl">本程序仅供参考，不承担任何责任。</li><li class="aboutl">作者微博：weibo.com/seatop</li><li class="aboutl">联系邮箱：seatop@sina.cn</li><li class="aboutl">源码下载：seatop.com.cn/pswd</li></ul></div>';
-		$("#app-right").html(outabout);        
+    $("footer").click(function(){
+    	$("nav li").css({"background":"#333","color":"#dad"});
+        outabout='<div id="p_ssa"><ul><li id="aboutt">关于</li><li class="aboutl">用记忆中的关键词生成无意义密码。</li><li class="aboutl">纯JS程序，MD5多重加密，可存至本地运行。</li><li class="aboutl">密码关键词为空时，生成随机密码。</li><li class="aboutl">附加关键词可用于区分不同的用途。</li><li class="aboutl">本程序仅供参考，不承担任何责任。</li><li class="aboutl">源码托管：github.com/seatop/miease.com</li><li class="aboutl">联系邮箱：seatop@sina.cn</li></ul></div>';
+		$("#app-content").html(outabout);        
     });
 
 	$(document).delegate('.s_font','mouseover',function(){
@@ -107,42 +107,42 @@ $(function(){
 	$(document).delegate('.n_font','click',function(){
 		pswdl=$(this).html();
 		outinputl='<div id="p_ss"><ul><li><h2 id="ps_l">当前密码长度为： '+pswdl+'</h2></li></ul></div>';
-		$("#app-right").html(outinputl+outn);
+		$("#app-content").html(outinputl+outn);
 	});
 
-	$("#app-left li:eq(0)").click(function(){
-		$("#app-right").html(outinput+outss);
+	$("nav li:eq(0)").click(function(){
+		$("#app-content").html(outinput+outss);
 		$("#ps1").focus();
 		cuob=$("#ps1");
 	});
 
-	$("#app-left li:eq(1)").click(function(){
-		$("#app-right").html(outinputk+outss);
+	$("nav li:eq(1)").click(function(){
+		$("#app-content").html(outinputk+outss);
 		$("#psk").focus();
 		cuob=$("#psk");
 	});
 
-	$("#app-left li:eq(2)").click(function(){
-		$("#app-right").html(outinputl+outn);		
+	$("nav li:eq(2)").click(function(){
+		$("#app-content").html(outinputl+outn);		
 	});
 
-	$("#app-left li:eq(3)").click(function(){
+	$("nav li:eq(3)").click(function(){
 		if(pswd==""){
 			outp=randomChar(pswdl);
-			$("#app-right").html('<div id="p_ss"><ul><li><h3>系统生成的随机密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请妥善保管！</h3></li></ul></div>');
+			$("#app-content").html('<div id="p_ss"><ul><li><h3>系统生成的随机密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请妥善保管！</h3></li></ul></div>');
 		}else{
 			if(pswdk==""){
 				var ptemp=hex_md5(pswd);
 				ptemp=passwordBit(ptemp,pswdl);
 				ptemp=hex_md5(ptemp);
 				outp=passwordBit(ptemp,pswdl);
-				$("#app-right").html('<div id="p_ss"><ul><li><h3>由密码关键词，系统生成的密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请记好密码关键词！</h3></li></ul></div>');
+				$("#app-content").html('<div id="p_ss"><ul><li><h3>由密码关键词，系统生成的密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请记好密码关键词！</h3></li></ul></div>');
 			}else{
 				var ptemp=hex_md5(pswd + pswdk);
 				ptemp=passwordBit(ptemp,pswdl);
 				ptmep=hex_md5(pswdk + ptemp + pswdk);
 				outp=passwordBit(ptmep,pswdl);
-				$("#app-right").html('<div id="p_ss"><ul><li><h3>由密码及附加关键词，系统生成的密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请记好两个关键词！</h3></li></ul></div>');
+				$("#app-content").html('<div id="p_ss"><ul><li><h3>由密码及附加关键词，系统生成的密码为</h2></li><li><h3>'+outp+'</h3></li><li><h3>如若使用此密码，请记好两个关键词！</h3></li></ul></div>');
 			}
 		}			
 	});
@@ -162,9 +162,9 @@ $(function(){
 		$("#btpsk").html('附加关键词输入成功！');	
 	});
 
-	$("#app-left li").click(function(){
-		$("#app-left li").css({"background":"#789","color":"#bcd"});
-		$(this).css({"background":"#bcd","color":"#789"});
+	$("nav li").click(function(){
+		$("nav li").css({"background":"#333","color":"#dad"});
+		$(this).css({"background":"#dcd","color":"#456"});
 	});
 
 });
